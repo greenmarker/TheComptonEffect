54a1a816900fa8161442b72c9f046d3d9bbcaea6
kamilkurop <kamilkurop@gmail.com>
2015-06-13 19:48:10 +0200

    Refactoring - extracted GUI components to methods

--- src/compton/ui/MenuCompton.java
+++ src/compton/ui/MenuCompton.java
@@ -1,7 +1,5 @@
 package compton.ui;
 
-
-
 import compton.animation.SinusoidComponent;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -12,44 +10,37 @@ import javax.swing.*;
 import net.miginfocom.swing.MigLayout;
 
 
-public class MenuCompton extends JFrame
-{
+public class MenuCompton extends JFrame {
 	final ResourceBundle bundle = PropertyResourceBundle.getBundle("compton");
-	
-	//Tworzenie obiektu
-	JFrame mainFrame = new JFrame(bundle.getString("app_title")); //Ramka Główna
-	
-	JPanel menuPanel = new JPanel(new MigLayout());
-	JPanel animationPanel = new JPanel(new MigLayout());
-	//Animation
-	SinusoidComponent sinusoid = new SinusoidComponent();
-	
-	OptionsPanel optionPanel = new OptionsPanel();
-	
 
-	public MenuCompton()//Konstruktor domyślny
-	{
+	public MenuCompton() {
 		//Ramka Główna
+		JFrame mainFrame = new JFrame(bundle.getString("app_title")); //Ramka Główna
 		mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE); // zamykanie ramki
 		mainFrame.setResizable(false);	//skalowanie ramki przez użytkownika
 		mainFrame.setVisible(true); //wywołanie ramki
 		mainFrame.setSize (800, 800); //rozmiar
 		mainFrame.setLayout(new MigLayout());
-		mainFrame.add(menuPanel,  "north");
-		mainFrame.add(animationPanel, "center");
-		mainFrame.add(optionPanel,"west,south");
-		
-		
-		//Pasek
-		
-		//Panel
-		menuPanel.add(new MenuBuilder().build());
-		
-		//Sinusioda
-		
-
-        
-        animationPanel.add(sinusoid);
+		mainFrame.add(createMenu(),  "north");
+		mainFrame.add(createAnimationPanel(), "center");
+		mainFrame.add(createOptionsPanel(),"west,south");
+		mainFrame.setVisible(true);
 	}
-	
+
+	private OptionsPanel createOptionsPanel() {
+		return new OptionsPanel();
+	}
+
+	private JPanel createMenu(){
+		JPanel menuPanel = new JPanel(new MigLayout());
+		menuPanel.add(new MenuBuilder().build());
+		return menuPanel;
+	}
+
+	private JPanel createAnimationPanel(){
+		SinusoidComponent sinusoid = new SinusoidComponent();
+		JPanel animationPanel = new JPanel(new MigLayout());
+		animationPanel.add(sinusoid);
+		return animationPanel;
+	}
 }
